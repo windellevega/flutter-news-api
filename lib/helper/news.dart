8db03a1bfa743c99784354b5ab09067726c61ec6
Windelle John Vega <windellevega@gmail.com>
2021-02-28 16:02:36 +0800

    Initial commit: Done features and functionalities (unclean code)

---
+++ lib/helper/news.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter_news_api/models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews() async {
+    String url =
+        'http://newsapi.org/v2/top-headlines?country=ph&category=business&apiKey=3699636a771049aca38ea30dd4ac1344';
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData['articles'].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          ArticleModel articleModel = new ArticleModel(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            content: element['content'],
+          );
+
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
+
+class CategoryNewsClass {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String category) async {
+    String url =
+        'http://newsapi.org/v2/top-headlines?category=$category&country=ph&category=business&apiKey=3699636a771049aca38ea30dd4ac1344';
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == 'ok') {
+      jsonData['articles'].forEach((element) {
+        if (element['urlToImage'] != null && element['description'] != null) {
+          ArticleModel articleModel = new ArticleModel(
+            title: element['title'],
+            author: element['author'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            content: element['content'],
+          );
+
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
